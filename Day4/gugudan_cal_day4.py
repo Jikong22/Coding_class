704b5f66a5af17e14ebeea4fd68cbad5ca8fd4ea
Jikong22 <7sfcre@gmail.com>
2025-08-22 13:31:59 +0900

    Day4

---
+++ Day4/gugudan_cal_day4.py
@@ -0,0 +1,19 @@
+dan = int(input("구구단을 몇 단을 계산할까요?(1~9)"))
+num = 0
+
+print("구구단 %s단을 계산합니다." %dan)
+while num <= 8:
+    num = num +1
+    print("%s x %s = %s" %(dan,num,dan*num))
+    continue
+
+
+
+
+
+
+
+
+
+
+
---
+++ Day4/if_covid_day4.py
@@ -0,0 +1,7 @@
+year = 0
+while year < 5:
+    year = year +1
+    print("코로나가 %d년째 입니다." %year)
+    if year == 5:
+        print("코로나가 끝났습니다.")
+
